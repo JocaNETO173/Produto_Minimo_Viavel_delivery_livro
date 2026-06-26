07331af34713a1199c21a7c5346e1f587cd491aa
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-06-26 12:40:04 -0300

    Add files via upload

---
+++ livros.js
@@ -0,0 +1,14 @@
+class livro{
+    constructor(ISBN, titulo, genero, preco){
+        this.ISBN = ISBN,
+        this.titulo = titulo,
+        this.genero = genero,
+        this.preco = preco
+    }
+}
+
+bd_livro
+
+function criarLivro(){
+
+}
