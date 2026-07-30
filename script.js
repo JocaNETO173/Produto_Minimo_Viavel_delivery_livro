e7208485b91d805000bb9bc69fbbde6a3662faf5
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-07-30 11:06:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const livros = [
+    {
+        id: 1,
+        capa: 'pequenoprincipe.jpg',
+        titulo: 'O Pequeno Príncipe',
+        autor: 'Antoine de Saint-Exupéry', 
+        categoria: 'Literatura Infantil'
+    },
+]
+
+const catalogo = document.getElementById('catalogo');
+const pesquisa = document.getElementById('pesquisa');
+
+function renderizarLivros(){
+    catalogo.innerHTML = '';
+
+    livros.forEach(livro =>{
+        catalogo.innerHTML += `
+            <div class="cards">
+                <img src="${livro.capa}" alt="${livro.titulo}_imagem">
+                <p class="titulo">${livro.titulo}</p>
+                <p class="autor">${livro.autor}</p>
+                <p class="categoria">${livro.categoria}</p>
+                <button>Adicionar ao Carrinho</button>
+            </div>
+            `
+    })
+}
+
+renderizarLivros()
